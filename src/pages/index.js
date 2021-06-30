479da69abcc961ba60c87ecda348e23c87d55e76
Andrew Combs <biwwlybam@Andrews-MacBook-Pro.local>
2021-06-30 18:13:57 -0400

    removed Abv calculator

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,6 @@ import "../styles/index.css";
 import { Helmet } from "react-helmet"
 
 import NavBar from "../components/NavBar/NavBar";
-import { Abv } from '../components/Abv/Abv';
 
 export default function Home() {
   return (
@@ -19,7 +18,7 @@ export default function Home() {
       <body>
         <div className="calculatorContainer">
           <div className="abvCalc">
-            <Abv />
+            
           </div>
         </div>
       </body>
