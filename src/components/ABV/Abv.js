cb175792da51c11857d7259d37ca0bea2b940512
Andrew Combs <biwwlybam@Andrews-MacBook-Pro.local>
2021-06-30 18:22:28 -0400

    remove export at start of Abv component

--- src/components/ABV/Abv.js
+++ src/components/ABV/Abv.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import '../../styles/Abv.css'
 
-export default class Abv extends React.Component {
+class Abv extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
